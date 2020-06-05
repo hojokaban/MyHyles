34054a1e37f9976636644703941050353a0c729c
hojokaban <hojokaban@gmail.com>
2020-06-05 19:53:31 +0000

    Add test
    contact と aboutへの遷移のテストを追加

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe "StaticPages", type: :request do
                 expect(response.status).to eq 200
             end
         end
+        context "このサイトについて画面が正しく表示される" do
+            it 'リクエストは200 OKとなること' do
+                get about_path
+                expect(response.status).to eq 200
+            end
+        end
+        context "お問い合わせ画面が正しく表示される" do
+            it 'リクエストは200 OKとなること' do
+                get contact_path
+                expect(response.status).to eq 200
+            end
+        end
     end
 end
