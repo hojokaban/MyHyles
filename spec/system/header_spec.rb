c15a451110c6ba5914885464d9b4e39a2acb2c56
hojokaban <hojokaban@gmail.com>
2020-06-06 21:04:38 +0000

    Add header_spec and modify header

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -7,7 +7,15 @@ describe 'ヘッダーのテスト', type: :system do
     context "ログインしていない状態" do
         it 'このサイトについてに遷移' do
             click_link 'このサイトについて'
-            expect(page).to have_content 'アバウトページ'
+            expect(page).to have_selector 'h2', text: 'アバウトページ'
+        end
+        it 'ログイン画面に遷移' do
+            click_link 'ログイン'
+            expect(page).to have_selector 'h2', text: 'ログイン'
+        end
+        it '新規登録画面に遷移' do
+            click_link '新規登録'
+            expect(page).to have_selector 'h2', text: '新規登録'
         end
     end
 
