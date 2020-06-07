8e6a8ab3d464e1c9105e76a3b7a6d3a79835a254
hojokaban <hojokaban@gmail.com>
2020-06-07 01:56:28 +0000

    Add confirmation mail

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     root 'static_pages#top'
     get '/about' => 'static_pages#about'
     get '/contact' => 'static_pages#contact'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
