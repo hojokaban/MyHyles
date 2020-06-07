33d961e0da97c291306cb8c8d83c6a97b2c03dbf
hojokaban <hojokaban@gmail.com>
2020-06-07 20:29:04 +0000

    Add password-reset move and refactor test

--- spec/system/devise_spec.rb
+++ spec/system/devise_spec.rb
@@ -1,49 +1,52 @@
 require 'rails_helper'
 describe 'deviseの統合テスト', type: :system do
 
-    context "新規登録機構" do
-        let(:user) { build(:user)}
-        before do
-            ActionMailer::Base.deliveries.clear
-        end
+    before do
+        ActionMailer::Base.deliveries.clear
+    end
+
+    context "新規登録前の動き" do
         it '新規登録に成功' do
             visit new_user_registration_path
             #情報を入力する
-            fill_in 'user_name', with: user.name
-            fill_in 'user_email', with: user.email
-            fill_in 'user_password', with: user.password
-            fill_in 'user_password_confirmation', with: user.password
+            fill_in 'user_name', with: "test"
+            fill_in 'user_email', with: "test@example.com"
+            fill_in 'user_password', with: "password"
+            fill_in 'user_password_confirmation', with: "password"
+            click_button '新規登録'
+
+            user = User.last
+            mail = open_email(user.email)
+
             #新規登録ボタンを押すとメールが送られる
-            expect{click_button '新規登録'}.to change { ActionMailer::Base.deliveries.size }.by(1)
+            mail.to deliver_to(user.email)
             #トップページへの遷移
             expect(page).to have_content 'アカウント有効化用のメールが送信されています'
 
             #ログインをすると失敗する
             click_link 'ログイン'
-            fill_in 'user_email', with: user.email
-            fill_in 'user_password', with: user.password
+            fill_in 'user_email', with: "test@example.com"
+            fill_in 'user_password', with: "password"
             click_button 'ログイン'
             expect(page).to have_content '作業を続行するにはアカウントを有効化する必要があります。'
             expect(page).not_to have_selector 'a', text: "#{user.name}さん"
 
-            signed_up_user = User.last
-            mail = open_email(signed_up_user.email)
             #メールの表示
-            mail.should have_body_text "#{signed_up_user.name}さん、ようこそ!"
+            mail.should have_body_text "#{user.name}さん、ようこそ!"
             click_email_link_matching(/http/, mail)
             #アカウントの有効化
             expect(page).to have_content 'メールアドレスが承認されました'
 
             #ログインが成功する
-            fill_in 'user_email', with: user.email
-            fill_in 'user_password', with: user.password
+            fill_in 'user_email', with: "test@example.com"
+            fill_in 'user_password', with: "password"
             click_button 'ログイン'
             expect(page).to have_content 'ログインしました!'
             expect(page).to have_selector 'a', text: "#{user.name}さん"
         end
     end
 
-    context "ログイン機構" do
+    context "新規登録後の動き" do
         let(:user) { create(:user, :confirmed)}
         before do
             visit new_user_session_path
@@ -61,8 +64,24 @@ describe 'deviseの統合テスト', type: :system do
             click_link 'パスワードを忘れた方はこちら'
             expect(page).to have_selector 'h2', text: 'パスワードをお忘れですか？'
             fill_in 'user_email', with: user.email
+            click_button 'パスワード再設定用のメールを送信'
             #ボタンを押すとメールが送られる
-            expect{click_button 'パスワード再設定用のメールを送信'}.to change { ActionMailer::Base.deliveries.size }.by(1)
+            mail = open_email(user.email)
+            mail.to deliver_to(user.email)
+            #トップページへの遷移
+            expect(page).to have_content 'パスワード再設定についてのメールが送信されます'
+            #メールの表示
+            mail.should have_body_text "#{user.name}さん、こんにちは!"
+            click_email_link_matching(/http/, mail)
+            #パスワード編集画面へ遷移
+            expect(page).to have_selector 'h2', text: 'パスワードの再設定'
+            #パスワードを変更する
+            fill_in 'user_password', with: "new_password"
+            fill_in 'user_password_confirmation', with: "new_password"
+            click_button 'パスワードを変更する'
+            #パスワード変更に成功し、ログインされる
+            expect(page).to have_content 'パスワードが変更されました。ログイン済みです。'
+            expect(page).to have_selector 'a', text: "#{user.name}さん"
         end
     end
 end
