53738472790f72920f60ca3cd9c5e2a502f3b3d4
hojokaban <hojokaban@gmail.com>
2020-06-08 00:43:32 +0000

    Generate user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
+    get 'users/show'
+    get 'users/edit'
+    devise_for :users, :controllers => {
                          :registrations => 'users/registrations',
                          :passwords => 'users/passwords'
                         }
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
     root 'static_pages#top'
     get '/about' => 'static_pages#about'
     get '/contact' => 'static_pages#contact'
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
