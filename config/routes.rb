c992461fa8cdc26b5ba17f15b4dcaf9b92da9b3e
hojokaban <hojokaban@gmail.com>
2020-06-08 00:46:11 +0000

    Add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
     root 'static_pages#top'
     get '/about' => 'static_pages#about'
     get '/contact' => 'static_pages#contact'
+    resource :users, only: [:show, :edit, :update]
 end
