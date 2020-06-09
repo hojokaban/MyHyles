128db3661121e201758bf4abe57e25d88c7469d9
hojokaban <hojokaban@gmail.com>
2020-06-09 06:06:44 +0000

    Generate hyle controller

---
+++ app/controllers/hyles_controller.rb
@@ -0,0 +1,29 @@
+class HylesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def categorized_index
+  end
+
+  def tagged_index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+end
