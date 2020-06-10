fc3d3b49861dcec0bf5f263e7593e4e13eb79c48
hojokaban <hojokaban@gmail.com>
2020-06-10 06:56:56 +0000

    Add model relationship between user and label

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,6 @@
 class Label < ApplicationRecord
     belongs_to :hyle
+    belongs_to :user
 
     validates :name, presence: true, length: {maximum: 20}
     validates :body, presence: true, length: {maximum: 500}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
     has_many :categories, dependent: :destroy
     has_many :hyles, dependent: :destroy
+    has_many :labels, dependent: :destroy
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
