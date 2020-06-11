fb0b3655e25d88432d6d9f0ee8280245a944da3b
hojokaban <hojokaban@gmail.com>
2020-06-11 13:36:28 +0000

    [test]Add memory edit

--- spec/system/memory_spec.rb
+++ spec/system/memory_spec.rb
@@ -14,22 +14,22 @@ describe 'memoriesの統合テスト', type: :system do
     visit new_users_memory_path
     expect(page).to have_selector 'h2', text: "思い出の追加"
     #思い出の追加に失敗
-    fill_in 'new-memory-title', with: "a"*21
+    fill_in 'memory-title', with: "a"*21
     click_button 'この内容で追加する'
     expect(page).to have_selector 'li', text: 'タイトルは20文字以内で入力してください'
     #思い出の追加に成功
-    fill_in 'new-memory-title', with: "test memory"
+    fill_in 'memory-title', with: "test memory"
     click_button 'この内容で追加する'
     expect(page).to have_content '思い出が追加されました!'
     #詳細画面に表示
     expect(page).to have_selector 'h2', text: "test memory"
     expect(page).to have_selector 'td', text: "test memory"
   end
-  it 'ヒュレーと思い出追加画面' do
+  it 'ヒュレーと思い出追加、そして編集' do
     hyle2 = create(:hyle, user:user, category:category)
     hyle3 = create(:hyle, user:user, category:category)
     visit new_users_memory_path
-    fill_in 'new-memory-title', with: "test memory"
+    fill_in 'memory-title', with: "test memory"
     check hyle2.name
     check hyle3.name
     click_button 'この内容で追加する'
@@ -39,5 +39,20 @@ describe 'memoriesの統合テスト', type: :system do
     expect(page).to have_selector 'td', text: hyle2.name
     expect(page).to have_selector 'td', text: hyle3.name
     expect(page).not_to have_selector 'td', text: hyle.name
+    #編集画面へ遷移
+    click_link '内容を編集する'
+    expect(page).to have_selector 'h2', text: "思い出の編集"
+    expect(page).to have_checked_field(hyle2.name)
+    expect(page).not_to have_checked_field(hyle.name)
+    #編集に失敗
+    fill_in 'memory-title', with: "a"*21
+    click_button '変更を保存する'
+    expect(page).to have_selector 'li', text: 'タイトルは20文字以内で入力してください'
+    #思い出の追加に成功
+    fill_in 'memory-title', with: "editted memory"
+    click_button '変更を保存する'
+    expect(page).to have_content '思い出が編集されました!'
+    #詳細画面に表示
+    expect(page).to have_selector 'td', text: "editted memory"
   end
 end
