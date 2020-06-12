9effc6ab4282e058ec64f077dfd13b902053a4d2
hojokaban <hojokaban@gmail.com>
2020-06-12 05:30:55 +0000

    Add validation to hyle_daily_relationship model

--- app/models/hyle_daily_relationship.rb
+++ app/models/hyle_daily_relationship.rb
@@ -1,4 +1,7 @@
 class HyleDailyRelationship < ApplicationRecord
   belongs_to :daily_relationship
   belongs_to :hyle
+
+  validates :relationship_amount,  presence: true, numericality: { less_than_or_equal_to: 100,
+              greater_than_or_equal_to: 0}
 end
--- spec/models/hyle_daily_relationship_spec.rb
+++ spec/models/hyle_daily_relationship_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe HyleDailyRelationship, type: :model do
+  context "HyleDailyRelationshipモデルのバリデーションが正しく稼働" do
+    let(:user) { create(:test_user) }
+    let(:category){create(:test_category, user:user)}
+    let(:hyle){create(:test_hyle, user:user, category:category)}
+    let(:daily_relationship){create(:daily_relationship, user:user)}
+    before do
+        @hyle_daily_relationship = HyleDailyRelationship.create!(
+                        hyle: hyle,
+                        daily_relationship: daily_relationship)
+    end
+    it "正しいヒュレー" do
+        expect(@hyle_daily_relationship).to be_valid
+    end
+    it "amountは空白ではいけない" do
+      @hyle_daily_relationship.relationship_amount = ""
+      expect(@hyle_daily_relationship).not_to be_valid
+    end
+    it "amountは0以上でないといけない" do
+      @hyle_daily_relationship.relationship_amount = -1
+      expect(@hyle_daily_relationship).not_to be_valid
+    end
+    it "amountは100以下でないといけない" do
+      @hyle_daily_relationship.relationship_amount = 101
+      expect(@hyle_daily_relationship).not_to be_valid
+    end
+  end
 end
