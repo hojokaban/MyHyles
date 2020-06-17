9f46722790304e9517a06b57da2dc4c5707338a1
hojokaban <hojokaban@gmail.com>
2020-06-17 11:15:06 +0000

    Layout hyle edit show

--- spec/system/hyle_spec.rb
+++ spec/system/hyle_spec.rb
@@ -215,7 +215,7 @@ describe 'hylesの統合テスト', type: :system do
         visit users_hyles_path
         expect(page).to have_selector 'h4', text: 'test_hyle'
         find("#hyle-link-#{hyle.id}").click
-        click_button '削除する'
+        click_link '削除する'
         accept_alert
         expect(page).to have_content "ヒュレーを削除しました"
         expect(page).to have_selector 'h2', text: '全ヒュレー'
