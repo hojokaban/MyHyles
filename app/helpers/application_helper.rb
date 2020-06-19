a0db3b53e641e228e221d41d9b260077652f5583
hojokaban <hojokaban@gmail.com>
2020-06-19 06:32:33 +0000

    Layout overall

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
         error_title = I18n.t("errors.messages.not_saved",
                    count: resource.errors.count,
                    resource: resource.class.model_name.human.downcase)
-        error_message = "<h2>#{error_title}</h2><ul>"
+        error_message = "<h3>#{error_title}</h3><ul>"
         resource.errors.full_messages.each { |message| error_message += "<li>" + message + "</li>" }
         return error_message + "</ul>"
     end
