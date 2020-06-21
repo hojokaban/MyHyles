5511f9e962ace7e4eeed9ff3724a21601493f529
hojokaban <hojokaban@gmail.com>
2020-06-21 15:33:01 +0000

    Modify batch

--- lib/batch/relationship.rb
+++ lib/batch/relationship.rb
@@ -1,10 +1,12 @@
 class Batch::Relationship
-    def self.relationship
-      User.all.each do |user|
-        user.has_done = false
-      end
-      Hyle.all.each do |hyle|
-        hyle.days_before(Date.current) unless hyle.birthday.nil?
-      end
+  def self.relationship
+    User.all.each do |user|
+      user.has_done = false
     end
+    p "関係を更新"
+    Hyle.all.each do |hyle|
+      hyle.days_before(Date.current) unless hyle.birthday.nil?
+    end
+    p "ヒュレーの誕生日カウントダウンを更新"
+  end
 end
