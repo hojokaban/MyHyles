03e5487793320e13b8bb3b3dac4f6b340e44a976
hojokaban <hojokaban@gmail.com>
2020-06-21 10:17:18 +0000

    Modify batch

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,28 +1,17 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require 'active_support/core_ext/time'
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+def jst(time)
+  Time.zone = 'Asia/Tokyo'
+  Time.zone.parse(time).localtime($system_utc_offset)
+end
+
 set :chronic_options, hours24: true
 # Learn more: http://github.com/javan/whenever
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
 set :environment, rails_env
 set :output, 'log/cron.log'
-every 1.day, at: '0:00' do
+every 1.day, at: jst('0:00') do
     begin
         runner "Batch::Relationship.relationship"
     rescue => e
