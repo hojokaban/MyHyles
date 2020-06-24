3fb589b9160507f8337d72cde8ab20e91f2ae94b
hojokaban <hojokaban@gmail.com>
2020-06-24 23:27:10 +0000

    Ajax user edit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,19 +42,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update
     self.resource = resource_class.to_adapter.get!(current_user.to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-    unless resource.term == params[:user][:term]
+    unless resource.term.to_s == params[:user][:term]
       resource.hyles.each{ |hyle| hyle.set_relationship(params[:user][:term]) }
       resource.set_relationship_percentage
     end
     resource_updated = update_resource(resource, account_update_params)
     yield resource if block_given?
     if resource_updated
-      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      flash.now[:success] = "アカウントが更新されました"
       bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
-      respond_with resource, location: after_update_path_for(resource)
+      #respond_with resource, location: after_update_path_for(resource)
     else
-      flash[:danger] = set_error_flash(resource) if resource.errors.any?
-      respond_with resource, location: after_update_path_for(resource)
+      flash.now[:danger] = set_error_flash(resource) if resource.errors.any?
+      #respond_with resource, location: after_update_path_for(resource)
     end
   end
 
