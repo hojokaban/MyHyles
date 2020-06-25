13cc581be3b96c7e0f86003efcff3973aa7a9116
hojokaban <hojokaban@gmail.com>
2020-06-25 00:23:33 +0000

    Ajax category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,28 +2,24 @@ include ApplicationHelper
 class CategoriesController < ApplicationController
 
     def create
-        @category = current_user.categories.new(category_params)
-        @category.save ? flash[:success] = "カテゴリーが追加されました" :
-            flash[:danger] = set_error_flash(@category)
-        redirect_back(fallback_location: root_path)
+      @category = current_user.categories.new(category_params)
+      @category.save ? flash.now[:success] = "カテゴリーが追加されました" :flash.now[:danger] = set_error_flash(@category)
+      @categories = current_user.categories
     end
 
     def update
-        @category = Category.find(params[:id])
-        @category.update(category_params) ? flash[:success] = "カテゴリーが編集されました"
-                : flash[:danger] = set_error_flash(@category)
-        redirect_back(fallback_location: root_path)
+      @category = Category.find(params[:id])
+      @category.update(category_params) ? flash.now[:success] = "カテゴリーが編集されました": flash.now[:danger] = set_error_flash(@category)
     end
 
     def destroy
-        Category.find(params[:id]).destroy
-        flash[:success] = "カテゴリーが削除されました"
-        redirect_back(fallback_location: root_path)
+      @category = Category.find(params[:id])
+      @category.destroy
+      flash.now[:success] = "カテゴリーが削除されました"
     end
 
     private
-
-        def category_params
-            params.require(:category).permit(:name)
-        end
+      def category_params
+          params.require(:category).permit(:name)
+      end
 end
